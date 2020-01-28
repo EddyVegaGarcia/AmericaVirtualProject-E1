02d5b00d04986e186e87c7665d5d819f0faf65f4
Eddy <eavg_93@outlook.com>
2020-01-28 03:18:24 -0300

    Se terminó de implementar el método de revisión de peligro.
    Primero se llamaba al dueño para revisar su economia.
    Luego se llamaba a revisar los recursos, uno era si el empleado se encontraba distraido, para posteriormente ver si había la cantidad adecuada de Uranio.
    Terminaba por llamar la exception de Planta Nuclear en peligro si ocurría: que el dueño esté pobre o el caso de las dos posibilidades, de uranio inestable y empleado distraido, sean correcta.
    En caso contrario de no lanzar la exception, terminaba con un mensaje Yujuu!!!

--- src/modelo/PlantaNuclear.java
+++ src/modelo/PlantaNuclear.java
@@ -1,5 +1,6 @@
 package modelo;
 
+import modelo.exception.PlantaNuclearEnPeligroException;
 import modelo.interfaces.*;
 
 import static modelo.Constantes.*;
@@ -7,14 +8,26 @@ import static modelo.Constantes.*;
 public class PlantaNuclear {
 
     EmpleadoDeControl empleadoDeControl;
-    public modelo.Burns dueño;
+    public modelo.Burns duenio;
     public Integer cantidadDeUranio;
+
+    private void monitorearEconomiaDuenio() {
+
+        this.duenio.estaPobre();
+
+    }
+
+    private void monitorearRecursos() {
+
+        this.empleadoDeControl.monitorear(this);
+
+    }
 
     public PlantaNuclear(Burns unDueño) {
 
         cantidadDeUranio = CANTIDAD_URANIO_INICIAL;
         this.empleadoDeControl = new Homero(CANTIDAD_DONAS_INICIALES);
-        this.dueño = unDueño;
+        this.duenio = unDueño;
 
     }
 
@@ -36,12 +49,17 @@ public class PlantaNuclear {
 
     }
 
-    public String revisionDePeligro() {
+    public void posibleCasoEnPeligro() {
 
-        this.dueño.estaPobre();
-
-        return NO_HAY_PELIGRO;
+        if(cantidadDeUranio >= CANTIDAD_URANIO_INESTABLE) throw new PlantaNuclearEnPeligroException();
 
     }
 
+    public String revisionDePeligro() {
+
+        this.monitorearEconomiaDuenio();
+        this.monitorearRecursos();
+        return NO_HAY_PELIGRO;
+
+    }
 }
