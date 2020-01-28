16278880ea7e5d6860109377faa9d4a17b8881e8
Eddy <eavg_93@outlook.com>
2020-01-27 23:15:16 -0300

    Agrego nuevas constantes.
    Se agrega las constantes cantidad de uranio inicial y el mensaje de todo OK.

--- src/modelo/Constantes.java
+++ src/modelo/Constantes.java
@@ -2,7 +2,9 @@ package modelo;
 
 public class Constantes {
 
+    public static Integer CANTIDAD_URANIO_INICIAL = 0;
     public static Integer CANTIDAD_URANIO_ESTABLE = 9000;
     public static Integer CANTIDAD_URANIO_INESTABLE = 10000;
+    public static String NO_HAY_PELIGRO = "Todo bien. Yujuu!!";
 
 }
