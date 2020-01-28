11d0ceca190205ee8dd518840d174abd6117c222
Eddy <eavg_93@outlook.com>
2020-01-28 00:45:40 -0300

    Se crea las clases de Homero y PatoBalancín para definir a los empleados y que deriven de la interfaz EmpleadoDeControl.
    Se crean por default y todavía no se a implementado nada.

---
+++ src/modelo/Homero.java
@@ -0,0 +1,14 @@
+package modelo;
+
+import modelo.interfaces.*;
+
+public class Homero implements EmpleadoDeControl {
+
+    public Integer donas;
+
+    public Homero(Integer unaCantidadDonas) {
+
+        this.donas = unaCantidadDonas;
+
+    }
+}
---
+++ src/modelo/PatoBalancin.java
@@ -0,0 +1,6 @@
+package modelo;
+
+import modelo.interfaces.*;
+
+public class PatoBalancin implements EmpleadoDeControl {
+}
