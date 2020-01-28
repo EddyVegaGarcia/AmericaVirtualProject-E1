7a316458bb19c922e2f098e2cb39e480c15cfdb4
Eddy <eavg_93@outlook.com>
2020-01-27 22:49:17 -0300

    Agregado de la clase Burns. Agregado de la interface EstadoBurns y clases derivadas Pobre y Rico.
    Se agregó la clase Burns y se crearon sus tipos de estados Economicos a lo cual estará.

---
+++ src/modelo/Burns.java
@@ -0,0 +1,13 @@
+package modelo;
+
+public class Burns {
+
+    EstadoBurns estadoBurns;
+
+    public Burns(EstadoBurns unEstadoEconomico) {
+
+        estadoBurns = unEstadoEconomico;
+
+    }
+
+}
---
+++ src/modelo/Pobre.java
@@ -0,0 +1,4 @@
+package modelo;
+
+public class Pobre implements EstadoBurns {
+}
