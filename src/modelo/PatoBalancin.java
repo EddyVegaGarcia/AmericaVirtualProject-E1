3b975fc8c98245a493a87519ed328306971b81ff
Eddy <eavg_93@outlook.com>
2020-01-28 03:15:07 -0300

    Se implementa el método de monitorear.
    Como el caso de Homero, la planta nuclear sabe que hay un posible caso de peligro. Aquí no es necesario saber si es false o true, sabemos que está distraido.

--- src/modelo/PatoBalancin.java
+++ src/modelo/PatoBalancin.java
@@ -6,11 +6,20 @@ public class PatoBalancin implements EmpleadoDeControl {
 
     @Override
     public Boolean estaDistraido() {
+
         return true;
+
     }
 
     @Override
     public void almorzar() {
+
+    }
+
+    @Override
+    public void monitorear(PlantaNuclear unaPlantaNuclear) {
+
+        unaPlantaNuclear.posibleCasoEnPeligro();
 
     }
 }
