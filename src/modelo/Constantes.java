dca76e029f2b2f596775e90bb9873bccc325fbb4
Eddy <eavg_93@outlook.com>
2020-01-28 00:20:14 -0300

    Se agregó las contantes de cantidad de Donas.
    Se agregó las donas iniciales que se le dará a Homero.

--- src/modelo/Constantes.java
+++ src/modelo/Constantes.java
@@ -5,6 +5,9 @@ public class Constantes {
     public static Integer CANTIDAD_URANIO_INICIAL = 0;
     public static Integer CANTIDAD_URANIO_ESTABLE = 9000;
     public static Integer CANTIDAD_URANIO_INESTABLE = 10000;
+
     public static String NO_HAY_PELIGRO = "Todo bien. Yujuu!!";
+
+    public static Integer CANTIDAD_DONAS_INICIALES = 5;
 
 }
