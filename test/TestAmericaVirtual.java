b6cf8d08e2046d96f34b5aa3aa5a6b8ab92efd26
Eddy <eavg_93@outlook.com>
2020-01-27 23:10:45 -0300

    Arreglo de los test's.
    Corrigo las creaciones de burns dentro de los parámetros al crear planta nuclear.

--- test/TestAmericaVirtual.java
+++ test/TestAmericaVirtual.java
@@ -10,172 +10,148 @@ public class TestAmericaVirtual {
     @Test
     public void revisionDePeligroConBurnsRicoConHomeroConcentradoYCantidadDeUranioEstable(){
 
-        Burns burns = new Burns(new Rico());
-
-        PlantaNuclear plantaNuclear = new PlantaNuclear(burns);
+        PlantaNuclear plantaNuclear = new PlantaNuclear(new Burns(new Rico()));
 
         plantaNuclear.agregarUranio(CANTIDAD_URANIO_ESTABLE);
 
-        assertEquals("Todo bien. Yujuu!!", plantaNuclear.revisionDePeligro);
+        assertEquals("Todo bien. Yujuu!!", plantaNuclear.revisionDePeligro());
 
     }
 
     @Test(expected = PlantaNuclearEnPeligroException.class)
     public void revisionDePeligroConBurnsPobreConHomeroConcentradoYCantidadDeUranioEstable(){
 
-        Burns burns = new Burns(new Pobre());
-
-        PlantaNuclear plantaNuclear = new PlantaNuclear(burns);
+        PlantaNuclear plantaNuclear = new PlantaNuclear(new Burns(new Pobre()));
 
         plantaNuclear.agregarUranio(CANTIDAD_URANIO_ESTABLE);
 
-        plantaNuclear.revisionDePeligro;
+        plantaNuclear.revisionDePeligro();
 
     }
 
     @Test(expected = PlantaNuclearEnPeligroException.class)
     public void revisionDePeligroConBurnsRicoConHomeroConcentradoYCantidadDeUranioInestable(){
 
-        Burns burns = new Burns(new Rico());
-
-        PlantaNuclear plantaNuclear = new PlantaNuclear(burns);
+        PlantaNuclear plantaNuclear = new PlantaNuclear(new Burns(new Rico()));
 
         plantaNuclear.agregarUranio(CANTIDAD_URANIO_INESTABLE);
 
-        plantaNuclear.revisionDePeligro;
+        plantaNuclear.revisionDePeligro();
 
     }
 
     @Test(expected = PlantaNuclearEnPeligroException.class)
     public void revisionDePeligroConBurnsPobreConHomeroConcentradoYCantidadDeUranioInestable(){
 
-        Burns burns = new Burns(new Pobre());
-
-        PlantaNuclear plantaNuclear = new PlantaNuclear(burns);
+        PlantaNuclear plantaNuclear = new PlantaNuclear(new Burns(new Pobre()));
 
         plantaNuclear.agregarUranio(CANTIDAD_URANIO_INESTABLE);
 
-        plantaNuclear.revisionDePeligro;
+        plantaNuclear.revisionDePeligro();
 
     }
 
     @Test(expected = PlantaNuclearEnPeligroException.class)
     public void revisionDePeligroConBurnsRicoConHomeroDistraidoYCantidadDeUranioEstable(){
 
-        Burns burns = new Burns(new Rico());
-
-        PlantaNuclear plantaNuclear = new PlantaNuclear(burns);
+        PlantaNuclear plantaNuclear = new PlantaNuclear(new Burns(new Rico()));
 
         plantaNuclear.agregarUranio(CANTIDAD_URANIO_ESTABLE);
 
         plantaNuclear.horaDeAlmuerzo();
 
-        plantaNuclear.revisionDePeligro;
+        plantaNuclear.revisionDePeligro();
 
     }
 
     @Test(expected = PlantaNuclearEnPeligroException.class)
     public void revisionDePeligroConBurnsPobreConHomeroDistraidoYCantidadDeUranioEstable(){
 
-        Burns burns = new Burns(new Pobre());
-
-        PlantaNuclear plantaNuclear = new PlantaNuclear(burns);
+        PlantaNuclear plantaNuclear = new PlantaNuclear(new Burns(new Pobre()));
 
         plantaNuclear.agregarUranio(CANTIDAD_URANIO_ESTABLE);
 
         plantaNuclear.horaDeAlmuerzo();
 
-        plantaNuclear.revisionDePeligro;
+        plantaNuclear.revisionDePeligro();
 
     }
 
     @Test(expected = PlantaNuclearEnPeligroException.class)
     public void revisionDePeligroConBurnsRicoConHomeroDistraidoYCantidadDeUranioInestable(){
 
-        Burns burns = new Burns(new Rico());
-
-        PlantaNuclear plantaNuclear = new PlantaNuclear(burns);
+        PlantaNuclear plantaNuclear = new PlantaNuclear(new Burns(new Rico()));
 
         plantaNuclear.agregarUranio(CANTIDAD_URANIO_INESTABLE);
 
         plantaNuclear.horaDeAlmuerzo();
 
-        plantaNuclear.revisionDePeligro;
+        plantaNuclear.revisionDePeligro();
 
     }
 
     @Test(expected = PlantaNuclearEnPeligroException.class)
     public void revisionDePeligroConBurnsPobreConHomeroDistraidoYCantidadDeUranioInestable(){
 
-        Burns burns = new Burns(new Pobre());
-
-        PlantaNuclear plantaNuclear = new PlantaNuclear(burns);
+        PlantaNuclear plantaNuclear = new PlantaNuclear(new Burns(new Pobre()));
 
         plantaNuclear.agregarUranio(CANTIDAD_URANIO_INESTABLE);
 
         plantaNuclear.horaDeAlmuerzo();
 
-        plantaNuclear.revisionDePeligro;
+        plantaNuclear.revisionDePeligro();
 
     }
 
     @Test
     public void revisionDePeligroConBurnsRicoConPatoBalancinYCantidadDeUranioEstable(){
 
-        Burns burns = new Burns(new Rico());
-
-        PlantaNuclear plantaNuclear = new PlantaNuclear(burns);
+        PlantaNuclear plantaNuclear = new PlantaNuclear(new Burns(new Rico()));
 
         plantaNuclear.agregarUranio(CANTIDAD_URANIO_ESTABLE);
 
         plantaNuclear.despido();
 
-        plantaNuclear.revisionDePeligro;
+        plantaNuclear.revisionDePeligro();
 
     }
 
     @Test(expected = PlantaNuclearEnPeligroException.class)
     public void revisionDePeligroConBurnsPobreConPatoBalancinYCantidadDeUranioEstable(){
 
-        Burns burns = new Burns(new Pobre());
-
-        PlantaNuclear plantaNuclear = new PlantaNuclear(burns);
+        PlantaNuclear plantaNuclear = new PlantaNuclear(new Burns(new Pobre()));
 
         plantaNuclear.agregarUranio(CANTIDAD_URANIO_ESTABLE);
 
         plantaNuclear.despido();
 
-        assertEquals("Todo mal. D'Ohhh!!!"plantaNuclear.revisionDePeligro);
+        plantaNuclear.revisionDePeligro();
 
     }
 
     @Test(expected = PlantaNuclearEnPeligroException.class)
     public void revisionDePeligroConBurnsRicoConPatoBalancinYCantidadDeUranioInestable(){
 
-        Burns burns = new Burns(new Rico());
-
-        PlantaNuclear plantaNuclear = new PlantaNuclear(burns);
+        PlantaNuclear plantaNuclear = new PlantaNuclear(new Burns(new Rico()));
 
         plantaNuclear.agregarUranio(CANTIDAD_URANIO_INESTABLE);
 
         plantaNuclear.despido();
 
-        plantaNuclear.revisionDePeligro;
+        plantaNuclear.revisionDePeligro();
 
     }
 
     @Test(expected = PlantaNuclearEnPeligroException.class)
     public void revisionDePeligroConBurnsPobreConPatoBalancinYCantidadDeUranioInestable(){
 
-        Burns burns = new Burns(new Pobre());
-
-        PlantaNuclear plantaNuclear = new PlantaNuclear(burns);
+        PlantaNuclear plantaNuclear = new PlantaNuclear(new Burns(new Pobre()));
 
         plantaNuclear.agregarUranio(CANTIDAD_URANIO_INESTABLE);
 
         plantaNuclear.despido();
 
-        plantaNuclear.revisionDePeligro;
+        plantaNuclear.revisionDePeligro();
 
     }
 
