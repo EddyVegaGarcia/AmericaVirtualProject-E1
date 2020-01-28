971888b6d0327300eb035c0e8ce979d2f700d406
Eddy <eavg_93@outlook.com>
2020-01-28 03:07:42 -0300

    Se agrega los método de monitorear.
    Su implementación ayuda a ver el caso si el empleado está distraido y así detectar si la planta nuclear está en peligro.

--- src/modelo/interfaces/EmpleadoDeControl.java
+++ src/modelo/interfaces/EmpleadoDeControl.java
@@ -1,9 +1,13 @@
 package modelo.interfaces;
 
+import modelo.PlantaNuclear;
+
 public interface EmpleadoDeControl {
 
     Boolean estaDistraido();
 
     void almorzar();
+
+    void monitorear(PlantaNuclear unaPlantaNuclear);
 
 }
